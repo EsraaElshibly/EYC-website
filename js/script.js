31438ca84540a882d9455f15805f646a8c6de8f9
EsraaElshibly <esraa.alshbly@gmail.com>
2022-01-09 16:20:19 +0200

    Second Commit

--- js/script.js
+++ js/script.js
@@ -4,10 +4,20 @@ function HandleClickToOpenPage ()
         window.open("./singlePage.html", "_self")
     }
 
-function OpenSingleMediaPage () {
-    location.replace("./SingleMediaPage.html" )
+function OpenSingleRadioPage () {
+    location.replace("./OpenSingleRadioPage.html" )
 }
 
 function OpenSingleFieldsPage() {
     location.replace("./SingleFieldsPage.html" )
+}
+
+function OpenSingleMediaPage () {
+    location.replace("./OpenSingleMediaPage.html" )
+}
+
+function HandleScroll () {
+    var nav = document.getElementById("nnav")
+    console.log("scroll");
+    nav.style.marginTop = "-50px"
 }
